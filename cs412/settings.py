f6d65d2409f00c63ac927b4b4e4694c3bf2ba43f
Leigh Brown <ljbrown4@bu.edu>
2026-01-27 10:46:10 -0500

    start of assignment 1

--- cs412/settings.py
+++ cs412/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hw', #NEW APP
-    #'quotes', # Assignment 1
+    'quotes', # Assignment 1
 ]
 
 MIDDLEWARE = [
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,25 @@
-# from django.shortcuts import render
-# from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# # Create your views here.
+import random
+# Create your views here.
+
+quotes = ["It is our choices, Harry, that show what we truly are, far more than our abilities,",
+          "It matters not what someone is born, but what they grow to be!",
+          "It is my belief... that the truth is generally preferable to lies." ]
+
+images = ['https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/07/24/17/Albus-Dumbledore.jpg',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4UTqWXbMXK01JULIJMJzy527DWJX8G0gi6w&s',
+          'https://hp-intothefire.wikidot.com/local--files/dumbledore-albus/Dumbledore.jpg']
+
+def home_page(request):
+    """ respond to url, delegate work to template"""
+
+    template_name = "quotes/home.html"
+
+    #dict of ctxt vars
+    context = {
+        'image': random.choice(images),
+        'quote': random.choice(quotes),
+    }
+    return render(request, template_name, context)
