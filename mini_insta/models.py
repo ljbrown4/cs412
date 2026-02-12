09f570b1e80df00659e2c1723d08b8a55405b151
Leigh Brown <ljbrown4@bu.edu>
2026-02-12 15:32:04 -0500

    fixed typo

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class Profile(models.Model):
     ''' Class that encapsulates data for user profiles'''
 
-    ursername = models.TextField(blank=True)
+    username = models.TextField(blank=True)
     display_name = models.TextField(blank=True)
     bio_text = models.TextField(blank=True)
     join_date = models.DateTimeField(auto_now=True)
     profile_image_url = models.URLField(blank=True)
 
     def __str__(self):
-        return f'{self.ursername} joined on {self.join_date}'
+        return f'{self.username} joined on {self.join_date}'
