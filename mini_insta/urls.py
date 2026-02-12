24a7ffb34054b840423c49d74730c1051c3a9fae
Leigh Brown <ljbrown4@bu.edu>
2026-02-12 15:33:03 -0500

    added appropiate path and method for viewing singular profile

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.conf import settings
 from . import views
-from .views import ProfileListView #ProfileDetailView
+from .views import ProfileListView, ProfileDetailView
  
  
 urlpatterns = [
     path('', ProfileListView.as_view(), name="show_all_profiles"),
-    # path('profile/<int:pk>', ProfileDetailView.as_view(), name='profile')
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name='profile')
 ] 
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -5,6 +5,14 @@ from .models import Profile
 # Create your views here.
 
 class ProfileListView(ListView):
+    ''' show all the profiles stored in the database'''
     model = Profile
     template_name = 'mini_insta/show_all_profiles.html'
     context_object_name = 'profiles'
+
+
+class ProfileDetailView(DetailView):
+    '''Show the information for one profle.'''
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile'
