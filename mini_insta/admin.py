61978e2d2d5e5e3b72393d465256cc2ed8e29988
Leigh Brown <ljbrown4@bu.edu>
2026-02-19 14:06:34 -0500

    updated models to include post and photo

--- mini_insta/admin.py
+++ mini_insta/admin.py
@@ -3,7 +3,9 @@
 # Description: connect class to admin
 
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Photo, Post
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Photo)
+admin.site.register(Post)
--- mini_insta/models.py
+++ mini_insta/models.py
@@ -1,6 +1,6 @@
 # File: models.py
 # Author: Leigh Brown (ljbrown@bu.edu), 2/12/2026
-# Description: create models and their attributes
+# Description: create models and their attributes (profile, post, photo)
 
 from django.db import models
 
@@ -17,3 +17,43 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.username} joined on {self.join_date}'
+    
+    def get_all_posts(self):
+        '''get all posts by a user'''
+        posts = Post.objects.filter(profile=self)
+        return posts
+    
+    
+
+class Post(models.Model):
+    '''class that encapsulates data for a user's post '''
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    caption = models.TextField(blank=True)
+
+    def __str__(self):
+        '''return string rep of this comment'''
+        return f'{self.profile} posted at {self.timestamp}'
+    
+    def get_all_photos(self):
+        '''get all photos for a post'''
+        photos = Photo.objects.filter(post=self)
+        return photos
+
+    def get_first_photo(self):
+        '''get 1st photo for a post'''
+        photos = Photo.objects.filter(post=self).first() #googled this
+        return photos
+
+class Photo(models.Model):
+    '''class that encapsulates data for a user's uploaded photos associated with a post '''
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        '''return string rep of this comment'''
+        return f'image uploaded at {self.timestamp} associated with post: {self.post}'
+
